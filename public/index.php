98a10ce55b6fa7a73f857cd31bf3bc2cfa63bfcf
Neizzzy <azat.azatch@yandex.ru>
2025-08-11 22:01:55 +0300

    refactor: migrate user storage to cookies

--- public/index.php
+++ public/index.php
@@ -21,9 +21,14 @@ function searchUsersByName(array $users, string $term): ?array
 {
     $lowerCaseTerm = mb_strtolower(trim($term));
     return array_filter($users, function ($user) use ($lowerCaseTerm) {
-        $lowerCaseName = mb_strtolower($user['name']);
+        $lowerCaseName = mb_strtolower($user['nickname']);
         return str_contains($lowerCaseName, $lowerCaseTerm);
     });
+}
+
+function getUsers($request)
+{
+    return json_decode($request->getCookieParam('users') ?? '', true);
 }
 
 $container = new Container();
@@ -46,8 +51,7 @@ $authMiddleware = function (Request $request, RequestHandler $handler) use ($rou
         $flash->addMessage('error', 'Access denied');
         $response = new \Slim\Psr7\Response();
 
-        return $response->withHeader('Location', $router->urlFor('index'))
-            ->withStatus(302);
+        return $response->withHeader('Location', $router->urlFor('index'))->withStatus(302);
     }
 
     return $handler->handle($request);
@@ -82,126 +86,138 @@ $app->delete('/logout', function ($request, $response) use ($router) {
     return $response->withRedirect($router->urlFor('index'), 302);
 });
 
-$app->group('', function (RouteCollectorProxy $group) use ($router) {
-    $group->group('/users', function (RouteCollectorProxy $users) use ($router) {
-        $userRepo = new UserRepository(__DIR__ . '/../assets/users.json');
+$app->get('/users', function ($request, $response) {
+    $messages = $this->get('flash')->getMessages();
+    $term = $request->getQueryParam('term', '');
+    $users = getUsers($request) ?? [];
 
-        $users->get('', function ($request, $response) use ($userRepo) {
-            $messages = $this->get('flash')->getMessages();
-            $term = mb_strtolower(trim($request->getQueryParam('term')));
-            $users = $userRepo->all();
+    $userResult = searchUsersByName($users, $term);
 
-            $filteredUsers = $term === ''
-                ? $users
-                : array_filter($users, fn($user) => str_contains(mb_strtolower($user['nickname']), $term));
+    $params = ['users' => $userResult, 'term' => $term, 'flash' => $messages];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+})->setName('users.index')->add($authMiddleware);
 
-            $params = ['users' => $filteredUsers, 'term' => $term, 'flash' => $messages];
-            return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-        })->setName('users.index');
+$app->get('/users/new', function ($request, $response) {
+    $params = [
+        'user' => [
+            'nickname' => '',
+            'email' => ''
+        ],
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/new.phtml', $params);
+})->setName('users.create')->add($authMiddleware);
 
-        $users->get('/new', function ($request, $response) {
-            $params = [
-                'user' => [
-                    'nickname' => '',
-                    'email' => ''
-                ],
-                'errors' => []
-            ];
-            return $this->get('renderer')->render($response, 'users/new.phtml', $params);
-        })->setName('users.create');
+$app->post('/users', function ($request, $response) use ($router) {
+    $userData = $request->getParsedBodyParam('user');
+    $validator = new UserValidator();
+    $errors = $validator->validate($userData);
 
-        $users->post('', function ($request, $response) use ($userRepo, $router) {
-            $user = $request->getParsedBodyParam('user');
-            $validator = new UserValidator();
-            $errors = $validator->validate($user);
+    if (count($errors) === 0) {
+        $users = getUsers($request);
+        $id = 1;
+        if (isset($users)) {
+            $lastUser = end($users);
+            $id += $lastUser['id'];
+        }
+        $userData['id'] = $id;
+        $users[$id] = $userData;
 
-            if (count($errors) === 0) {
-                $userRepo->create($user);
-                $this->get('flash')->addMessage('success', 'User was added successfully');
-                $url = $router->urlFor('users.index');
-                return $response->withRedirect($url, 302);
-            }
+        $encodedUsers = json_encode($users);
+        $this->get('flash')->addMessage('success', 'User was added successfully');
 
-            $params = [
-                'user' => $user,
-                'errors' => $errors
-            ];
-            return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
-        })->setName('users.store');
+        return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
+            ->withRedirect($router->urlFor('users.index'), 302);
+    }
 
-        $users->get('/{id}', function ($request, $response, array $args) use ($userRepo) {
-            $id = $args['id'];
+    $params = [
+        'user' => $userData,
+        'errors' => $errors
+    ];
+    return $this->get('renderer')->render($response->withStatus(422), 'users/new.phtml', $params);
+})->setName('users.store')->add($authMiddleware);
 
-            $user = $userRepo->findById($id);
-            if (!$user) {
-                return $response->write('User not found!')->withStatus(404);
-            }
+$app->get('/users/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $user = getUsers($request)[$id];
+    if (!$user) {
+        return $response->write('User not found!')->withStatus(404);
+    }
 
-            $params = ['user' => $user];
-            return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-        })->setName('users.show');
+    $params = ['user' => $user];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+})->setName('users.show')->add($authMiddleware);
 
-        $users->get('/{id}/edit', function ($request, $response, array $args) use ($userRepo) {
-            $id = $args['id'];
-            $user = $userRepo->findById($id);
-            if (!$user) {
-                return $response->write('User not found!')->withStatus(404);
-            }
+$app->get('/users/{id}/edit', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $user = getUsers($request)[$id];
+    if (!$user) {
+        return $response->write('User not found!')->withStatus(404);
+    }
 
-            $params = [
-                'user' => $user,
-                'errors' => []
-            ];
-            return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
-        })->setName('users.edit');
+    $params = [
+        'user' => $user,
+        'errors' => []
+    ];
+    return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
+})->setName('users.edit')->add($authMiddleware);
 
-        $users->patch('/{id}', function ($request, $response, array $args) use ($userRepo, $router) {
-            $id = $args['id'];
-            $data = $request->getParsedBodyParam('user');
-            $validator = new UserValidator;
-            $errors = $validator->validate($data);
+$app->patch('/users/{id}', function ($request, $response, array $args) use ($router) {
+    $id = $args['id'];
+    $userData = $request->getParsedBodyParam('user');
 
-            if (count($errors) === 0) {
-                $userRepo->update($id, $data);
-                $url = $router->urlFor('users.index');
-                $this->get('flash')->addMessage('success', 'User was updated successfully');
-                return $response->withRedirect($url, 302);
-            }
+    $users = getUsers($request);
+    $user = $users[$id];
+    if (!$user) {
+        return $response->write('User not found!')->withStatus(404);
+    }
 
-            $user = $userRepo->findById($id);
-            if (!$user) {
-                return $response->write('User not found!')->withStatus(404);
-            }
-            $params = [
-                'user' => $user,
-                'errors' => $errors
-            ];
-            return $this->get('renderer')->render($response, 'users/edit.phtml', $params);
-        })->setName('users.update');
+    $validator = new UserValidator;
+    $errors = $validator->validate($userData);
 
-        $users->get('/{id}/delete', function ($request, $response, array $args) use ($userRepo) {
-            $id = $args['id'];
-            $user = $userRepo->findById($id);
-            if (!$user) {
-                return $response->write('User not found!')->withStatus(404);
-            }
+    if (count($errors) === 0) {
+        $user['nickname'] = $userData['nickname'];
+        $user['email'] = $userData['email'];
+        $users[$id] = $user;
 
-            $params = [
-                'user' => $user
-            ];
-            return $this->get('renderer')->render($response, 'users/delete.phtml', $params);
-        });
+        $encodedUsers = json_encode($users);
+        $this->get('flash')->addMessage('success', 'User was updated successfully');
 
-        $users->delete('/{id}', function ($request, $response, array $args) use ($userRepo, $router) {
-            $id = $args['id'];
-            $userRepo->destroy($id);
+        return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
+            ->withRedirect($router->urlFor('users.index'), 302);
+    }
 
-            $this->get('flash')->addMessage('success', 'User was deleted successfully');
-            return $response->withRedirect($router->urlFor('users.index'), 302);
-        });
-    });
+    $userData['id'] = $id;
+    $params = [
+        'user' => $userData,
+        'errors' => $errors
+    ];
+    return $this->get('renderer')->render($response->withStatus(422), 'users/edit.phtml', $params);
+})->setName('users.update')->add($authMiddleware);
 
-    //Такие же обработчики для cars
-})->add($authMiddleware);
+$app->get('/users/{id}/delete', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $user = getUsers($request)[$id];
+    if (!$user) {
+        return $response->write('User not found!')->withStatus(404);
+    }
+
+    $params = [
+        'user' => $user
+    ];
+    return $this->get('renderer')->render($response, 'users/delete.phtml', $params);
+})->setName('users.delete')->add($authMiddleware);
+
+$app->delete('/users/{id}', function ($request, $response, array $args) use ($router) {
+    $id = $args['id'];
+    $users = getUsers($request);
+    unset($users[$id]);
+
+    $encodedUsers = json_encode($users);
+    $this->get('flash')->addMessage('success', 'User was deleted successfully');
+
+    return $response->withHeader('Set-Cookie', "users={$encodedUsers};path=/")
+        ->withRedirect($router->urlFor('users.index'), 302);
+})->setName('users.destroy')->add($authMiddleware);
 
 $app->run();
